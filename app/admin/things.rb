d6cd977de288764c2b4b4b8f8706939db37a1adc
Raghu Betina <raghu@firstdraft.com>
2020-12-02 20:22:48 +0000

    Whitelist columns in Thing for ActiveAdmin

--- app/admin/things.rb
+++ app/admin/things.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Thing do
 
+ permit_params :description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
